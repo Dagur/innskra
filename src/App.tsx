d3b51b1be82b462c99b1bcfcb24bbbdca883f98e
Dagur Ammendrup <dagurp@vivaldi.com>
2020-10-21 11:35:36 +0000

    Second attempt

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import React, { FormEvent } from 'react';
 import './App.css';
 
+const encode = (data: any) => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
+
 function App() {
   const [name, setName] = React.useState("");
-
-  const encode = (data: any) => {
-    return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-  }
+  const [message, setMessage] = React.useState("");
 
   const handleSubmit = (event: FormEvent) => {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "innskra", name })
+      body: encode({ "form-name": "innskra", name, message })
     })
-      .then(() => alert("Takk fyrir og góða skemmtun"))
+      .then(response => {
+        alert("Takk fyrir og góða skemmtun")
+      })
       .catch(error => alert(error));
 
     event.preventDefault();
@@ -36,6 +39,15 @@ function App() {
             />
           </label>
         </p>
+        <p>
+            <label>
+              Skilaboð (valfrjálst): <textarea 
+                name="message" 
+                value={message} 
+                onChange={event => setMessage(event.target.value)} 
+              />
+            </label>
+          </p>
         <p>
           <button type="submit">Innskrá</button>
         </p>
